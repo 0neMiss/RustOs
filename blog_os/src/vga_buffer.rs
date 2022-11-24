ae5ff6f9eaebaf03c9d5c45d20151c83c1cb979b
0neMiss <jordanshehane@gmail.com>
2022-11-24 11:59:32 -0700

    comments

--- blog_os/src/vga_buffer.rs
+++ blog_os/src/vga_buffer.rs
@@ -30,6 +30,7 @@ impl ColorCode {
     }
 }
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+//repr(C) allows us to enforce enum order
 #[repr(C)]
 struct ScreenChar {
     ascii_character: u8,
@@ -38,7 +39,8 @@ struct ScreenChar {
 
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
-
+// repr transparent is support for some type recasting that happens between C and rust(i think)
+// https://doc.rust-lang.org/1.26.2/unstable-book/language-features/repr-transparent.html#enter-reprtransparent
 #[repr(transparent)]
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT]
@@ -54,22 +56,28 @@ impl Writer {
     pub fn write_string(&mut self, s: &str){
         for byte in s.bytes(){
             match byte {
+                // if the bite is an ASCII character then write it
                 0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Otherwise write █
                 _ => self.write_byte(0xfe),
             }
         }
     }
     pub fn write_byte(&mut self, byte: u8) {
         match byte {
+            // if the byte is of the type new line, create  new line
             b'\n' => self.new_line(),
             byte => {
+                // if the current width of our text is too wide, overflow this char to next line
                 if self.column_position >= BUFFER_WIDTH {
                     self.new_line()
                 }
+
                 let row = BUFFER_HEIGHT -1;
                 let col = self.column_position;
 
                 let color_code = self.color_code;
+                // inside the buffer, create a entry for the character we are writing to the screen.
                 self.buffer.chars[row][col] = ScreenChar {
                     ascii_character: byte,
                     color_code
@@ -82,6 +90,12 @@ impl Writer {
     fn new_line(&mut self) {}
 }
 pub fn print_something(){
+    // create a writer that points to the VGA buffer at 0xb8000
+    /* . The syntax for this might seem a bit strange: 
+    First, we cast the integer 0xb8000 as a mutable raw pointer. 
+    Then we convert it to a mutable reference by dereferencing it (through *) 
+    and immediately borrowing it again (through &mut). */
+
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Green, Color::Black),
